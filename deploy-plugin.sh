30407ab64df2bbbc35953e221804c76c9382cd89
sksat <sksat@sksat.net>
2021-09-11 00:05:15 +0900

    add: plugin deploy script

---
+++ deploy-plugin.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+COREPROTECT_REPO="PlayPro/CoreProtect"
+DISCORDSRV_REPO="DiscordSRV/DiscordSRV"
+PROMETHEUS_EXPORTER_REPO="sladkoff/minecraft-prometheus-exporter"
+
+COREPROTECT_VERSION="20.0"
+DISCORDSRV_VERSION="1.23.0"
+PROMETHEUS_EXPORTER_VERSION="2.4.2"
+
+COREPROTECT_FPREFIX="CoreProtect"
+DISCORDSRV_FPREFIX="DiscordSRV-Build"
+PROMETHEUS_EXPORTER_FPREFIX="minecraft-prometheus-exporter"
+
+function download_github(){
+	REPO="$1"
+	VERSION="$2"
+	FNAME_PREFIX="$3"
+
+	cd data/plugins
+	echo "remove old version"
+	rm "${FNAME_PREFIX}"*
+	echo "download new version"
+	wget "https://github.com/${REPO}/releases/download/v${VERSION}/${FNAME_PREFIX}-${VERSION}.jar"
+	cd ../..
+}
+
+download_github "${COREPROTECT_REPO}" "${COREPROTECT_VERSION}" "${COREPROTECT_FPREFIX}"
+download_github "${DISCORDSRV_REPO}" "${DISCORDSRV_VERSION}" "${DISCORDSRV_FPREFIX}"
+download_github "${PROMETHEUS_EXPORTER_REPO}" "${PROMETHEUS_EXPORTER_VERSION}" "${PROMETHEUS_EXPORTER_FPREFIX}"
